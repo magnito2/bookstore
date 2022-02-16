3c6ac60e4ea251f2d9bc426d3ed0152e90f0572a
magnito2 <magnusotwani@gmail.com>
2022-02-16 04:55:06 +0300

    make responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { signOutUserStart } from "../../redux/User/user.actions";
@@ -6,6 +6,8 @@ import { selectCartItemsCount } from "../../redux/Cart/cart.selectors";
 import "./styles.scss";
 
 import Logo from "../../assets/Logo.png";
+import {ReactComponent as CloseMenu} from "../../assets/x.svg";
+import {ReactComponent as MenuIcon} from "../../assets/menu.svg"
 
 const mapState = (state) => ({
   currentUser: state.user.currentUser,
@@ -15,6 +17,10 @@ const mapState = (state) => ({
 const Header = (props) => {
   const dispatch = useDispatch();
   const { currentUser, totalNumCartItems } = useSelector(mapState);
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
 
   const signOut = () => {
     dispatch(signOutUserStart());
@@ -27,43 +33,55 @@ const Header = (props) => {
             <img src={Logo} alt="ekitabu Logo" />
           </Link>
         </div>
-
         <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/search">Search</Link>
-            </li>
-          </ul>
-        </nav>
+          <ul className={click ? "nav-options active" : "nav-options"}>
+            <div className="navigators">
+              <li className="option" onClick={closeMobileMenu}>
+                <Link to="/">Home</Link>
+              </li>
+              <li className="option" onClick={closeMobileMenu}>
+                <Link to="/search">Search</Link>
+              </li>
 
-        <div className="callToActions">
-          <ul>
-            <li>
-              <Link to="/cart">
-                Your Cart ({ totalNumCartItems })
-              </Link>
-            </li>
-              {currentUser && [
-                  <li>
+              <li className="option" onClick={closeMobileMenu}>
+                <Link to="/cart">
+                  Your Cart ({ totalNumCartItems })
+                </Link>
+              </li>
+            </div>
+            <div className="accountActions">
+            {currentUser && [
+                  <li className="option" onClick={closeMobileMenu}>
                     <Link to="/dashboard">My Account</Link>
                   </li>,
-                  <li>
+                  <li className="option" onClick={closeMobileMenu}>
                     <span onClick={() => signOut()}>Logout</span>
                   </li>
               ]}
               {!currentUser && [
-                  <li>
+                  <li className="option" onClick={closeMobileMenu}>
                     <Link to="/registration">Register</Link>
                   </li>,
-                  <li>
+                  <li className="option" onClick={closeMobileMenu}>
                     <Link to="/login">Login</Link>
                   </li>
               ]}
+            </div>
+          </ul>
+        </nav>
+
+        <div className="callToActions">
+          <ul className={click ? "nav-options active" : "nav-options"}>
+            
           </ul>
         </div>
+        <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+      </div>
       </div>
     </header>
   );
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 
 import Directory from "../../components/Directory";
+import ProductResults from "../../components/ProductResults";
 
 import "./styles.scss";
 
 const Homepage = props => {
     return (
         <section className="homepage">
-            <Directory />
+            <ProductResults />
         </section>
     );
 };
